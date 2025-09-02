b0cfdd571cbf2b4f30a6102d9ac503264ed71ed5
Jashua <jashua.bautistaobina@gmail.com>
2025-09-02 16:54:39 +0800

    feat(app): add React Query provider and integrate with routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { lazy, Suspense } from "react"
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient()
 
 const Authentication = lazy(() => import('./pages/Authentication/Authentication'))
 const AttendanceRecord = lazy(() => import('./pages/AttendanceRecord/AttendanceRecord'))
@@ -9,10 +12,12 @@ function App() {
   return (
     <Router>
       <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path="/authentication" element={<Authentication />}/>
-          <Route path="/attendance-record" element={<AttendanceRecord />}/>
-        </Routes>
+        <QueryClientProvider client={queryClient}>
+          <Routes>
+            <Route path="/authentication" element={<Authentication />}/>
+            <Route path="/attendance-record" element={<AttendanceRecord />}/>
+          </Routes>
+        </QueryClientProvider>
       </Suspense>
     </Router>
   )
