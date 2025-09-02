03d6c71f285ae5014549a7988a86da094a010a62
Jashua <jashua.bautistaobina@gmail.com>
2025-09-02 14:45:06 +0800

    feat(auth): create Authentication component with styled login layout

--- src/pages/Authentication/Authentication.jsx
+++ src/pages/Authentication/Authentication.jsx
@@ -1,7 +1,41 @@
+import "./Authentication.css"
+
 const Authentication = () => {
   return (
-    <h1>Employee Time In/Time Out/Sign In Here!!!</h1>
-  )
+    <>
+    <div class="container">
+      <div class="left-panel">
+        <img src="Wib.png" alt="When in Baguio Logo" class="main-logo"/>
+        <div class="blob-shape1"></div>
+        <div class="blob-shape2"></div>
+    </div>
+
+  <div class="right-panel">
+    <a href="index.html" class="time-in-header">Time In</a>
+
+      <div class="logo-section">
+        <img src="WIB LOGO.png" class="baguio-logo" />
+      </div>
+      
+      <form class="login-form" onsubmit="handleLogin(event)">
+        <div class="form-group">
+          <input type="email" id="email" placeholder="Email" required/>
+        </div>
+        
+        <div class="form-group">
+          <input type="password" id="password" placeholder="Password" required/>
+        </div>
+        
+        <a href="#" class="forgot-password">Forgot Password?</a>
+        
+        <a href="employeedashboard.html" class="btn-login">Login</a>
+
+      </form>
+    </div>
+  </div>
+
+  </>
+)
 }
 
 export default Authentication
