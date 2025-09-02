5784868f877077d810b1dba2aaacdfcbec67ccb6
John Lino <143068921+s3lftitled@users.noreply.github.com>
2025-09-02 14:54:56 +0800

    Update Authentication.jsx

--- src/pages/Authentication/Authentication.jsx
+++ src/pages/Authentication/Authentication.jsx
@@ -3,36 +3,36 @@ import "./Authentication.css"
 const Authentication = () => {
   return (
     <>
-    <div class="container">
-      <div class="left-panel">
-        <img src="Wib.png" alt="When in Baguio Logo" class="main-logo"/>
-        <div class="blob-shape1"></div>
-        <div class="blob-shape2"></div>
-    </div>
-
-  <div class="right-panel">
-    <a href="index.html" class="time-in-header">Time In</a>
-
-      <div class="logo-section">
-        <img src="WIB LOGO.png" class="baguio-logo" />
-      </div>
-      
-      <form class="login-form" onsubmit="handleLogin(event)">
-        <div class="form-group">
-          <input type="email" id="email" placeholder="Email" required/>
-        </div>
-        
-        <div class="form-group">
-          <input type="password" id="password" placeholder="Password" required/>
-        </div>
-        
-        <a href="#" class="forgot-password">Forgot Password?</a>
-        
-        <a href="employeedashboard.html" class="btn-login">Login</a>
-
-      </form>
-    </div>
+   <div className="container">
+  <div className="left-panel">
+    <img src="Wib.png" alt="When in Baguio Logo" className="main-logo" />
+    <div className="blob-shape1"></div>
+    <div className="blob-shape2"></div>
   </div>
+
+  <div className="right-panel">
+    <a href="index.html" className="time-in-header">Time In</a>
+
+    <div className="logo-section">
+      <img src="WIB LOGO.png" className="baguio-logo" alt="WIB Logo" />
+    </div>
+
+    <form className="login-form" onSubmit={handleLogin}>
+      <div className="form-group">
+        <input type="email" id="email" placeholder="Email" required />
+      </div>
+
+      <div className="form-group">
+        <input type="password" id="password" placeholder="Password" required />
+      </div>
+
+      <a href="#" className="forgot-password">Forgot Password?</a>
+
+      <a href="employeedashboard.html" className="btn-login">Login</a>
+    </form>
+  </div>
+</div>
+
 
   </>
 )
