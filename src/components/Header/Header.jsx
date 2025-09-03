8273a8cf90ac2ec4d0245519952a5e12b148e336
calcifeerrr <ortiguerofrances67@gmail.com>
2025-09-03 13:55:23 +0800

    feat(header): replace emoji logo with When in Baguio image logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -31,7 +31,14 @@ const Header = ({
             Welcome, <span id="userName">Juan Dela Cruz</span>!
           </div>
         </div>
-        <div className="logo">When in Baguio</div>
+        <div className="logo">
+  <img 
+    src="WIB LOGO.png" 
+    alt="When in Baguio Logo" 
+    className="logo-img" 
+  />
+</div>
+
       </header>
 
       <nav className="navigation">
